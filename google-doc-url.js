e9fbd15fd6b2ba38c67422712feb117a1e8c1241
Sasha Brown <sashadbrown@gmail.com>
2020-04-05 14:22:29 +0100

    google sheet

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yQzHrmHPEL51QDOHnF2kP3v4phNSqMYsTqP6RsJZIPY/edit#gid=0';
